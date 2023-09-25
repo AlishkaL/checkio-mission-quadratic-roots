39fa2d20089c38bacf0ca013e033bc937f0216fe
Oleksandr Zozulia <freemanlex@gmail.com>
2023-09-25 16:17:10 +0300

    upd

--- verification/tests.py
+++ verification/tests.py
@@ -10,19 +10,19 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {"input": [1, -3, 2], "answer": (2, 1)},
-        {"input": [1, 0, -1], "answer": (1, -1)},
-        {"input": [1, 2, 1], "answer": (-1, -1)},
-        {"input": [1, 0, 1], "answer": "No real roots"},
-        {"input": [1, 4, 4], "answer": (-2, -2)},
-        {"input": [1, -5, 6], "answer": (3, 2)},
-        {"input": [1, -6, 9], "answer": (3, 3)},
-        {"input": [2, 2, 1], "answer": "No real roots"},
-        {"input": [2, -7, 6], "answer": (2, 1.5)},
-        {"input": [2, -3, 1], "answer": (1, 0.5)},
+        {"input": [1, -3, 2], "answer": [2, 1]},
+        {"input": [1, 0, -1], "answer": [1, -1]},
+        {"input": [1, 2, 1], "answer": [-1, -1]},
+        {"input": [1, 0, 1], "answer": ["No real roots"]},
+        {"input": [1, 4, 4], "answer": [-2, -2]},
+        {"input": [1, -5, 6], "answer": [3, 2]},
+        {"input": [1, -6, 9], "answer": [3, 3]},
+        {"input": [2, 2, 1], "answer": ["No real roots"]},
+        {"input": [2, -7, 6], "answer": [2, 1.5]},
+        {"input": [2, -3, 1], "answer": [1, 0.5]},
     ],
     "Extra": [
-        {"input": [1, 7, 15], "answer": "No real roots"},
-        {"input": [1, -8, 15], "answer": (5, 3)}
+        {"input": [1, 7, 15], "answer": ["No real roots"]},
+        {"input": [1, -8, 15], "answer": [5, 3]}
     ]
 }
